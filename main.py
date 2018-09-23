8f6ae41ecad86f8b0ae8d96ee0b36c5949e684d7
Mahmoud Hashim <mahmoud@aims.ac.za>
2018-09-23 20:41:23 +0200

    simulation sec update

--- main.py
+++ main.py
@@ -18,8 +18,12 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.image import Image
 from kivy.uix.filechooser import FileChooserListView, FileChooserIconView
 from kivy.uix.camera import Camera
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.spinner import Spinner
+
 
 from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.behaviors import ToggleButtonBehavior
 
 from kivy.garden.graph import Graph, MeshLinePlot
 from kivy.uix.progressbar import ProgressBar
@@ -39,6 +43,9 @@ from matplotlib.lines import Line2D
 
 import glob
 import subprocess, shlex
+
+from textdictENG import text_dict
+
 
 #from kivy.core.window import Window
 #Window.clearcolor = (1, 1, 1, 1)
@@ -75,7 +82,7 @@ class MyApp(App):
         self.ax = plt.Axes(self.fig, [0., 0., 1., 1.]); self.ax.set_axis_off(); self.fig.add_axes(self.ax)
         self.canvas = FigureCanvasKivyAgg(self.fig)
         self.img=mpimg.imread('BBF_frontpage.png')
-        self.ax.imshow(self.img)
+        self.ax.imshow(self.img); self.anim_running = True
         
         #--: Widgets
         btn_user = IconButton(source='./icons/user.png', text='', size_hint_x=None, width=50)
@@ -90,60 +97,101 @@ class MyApp(App):
         
         btn_home = IconButton(source='./icons/home.png', size_hint_x = None, width = 50)
         btn_home.bind(on_release=self.clean)
-
+        
+        self.btn_pause = IconButton(source='./icons/pause.png', size_hint_x = None, width = 50)
+        self.btn_pause.bind(on_press=self.pause)
 
         #--:Page Layout
         #--- Page 1
         Pages = PageLayout(orientation= "vertical")
         Pages.add_widget(self.canvas)
         #--- Page 2
-        Settings_Page = GridLayout(cols=1, row_force_default=True, row_default_height=40)
+        self.Settings_Page = GridLayout(cols=1, row_force_default=True, row_default_height=40)
         
-        Settings_Page.add_widget(btn_user)
-        Settings_Page.add_widget(btn_sim)
-        Settings_Page.add_widget(btn_lens)
-        Settings_Page.add_widget(btn_settings)
-        Settings_Page.add_widget(btn_home)
+        self.Settings_Page.add_widget(btn_user)
+        self.Settings_Page.add_widget(btn_sim)
+        self.Settings_Page.add_widget(btn_lens)
+        self.Settings_Page.add_widget(btn_settings)
+        self.Settings_Page.add_widget(btn_home)
         
-        Pages.add_widget(Settings_Page)
+        Pages.add_widget(self.Settings_Page)
         return Pages
     
     def show_popup_user(self,*args):
         label_name = Image(source='./icons/name.png')
-        self.input_name = TextInput(text='', multiline=False, size_hint_x = None, width = 350)
+        self.input_name = TextInput(text='', multiline=False, size_hint_x = None, width = 200)
         
         label_email = Image(source='./icons/email.png')
-        input_email = TextInput(text='', multiline=False, size_hint_x = None, width = 350)
+        input_email = TextInput(text='', multiline=False, size_hint_x = None, width = 200)
         
-
         self.btn_cam = IconButton(source='./icons/photo.png')
         self.btn_cam.bind(on_release=self.show_popup_cam)
         
-        Settings_content = BoxLayout(orientation='vertical')
-        Settings_content.add_widget(self.btn_cam)
-        user_content = GridLayout(cols = 2, size_hint_y = None, height = '68dp')
+        Settings_content = BoxLayout(orientation='horizontal')
+        user_content = GridLayout(cols = 2, size_hint_y = None, height = '58dp')
+        
         user_content.add_widget(label_name)
         user_content.add_widget(self.input_name)
         user_content.add_widget(label_email)
         user_content.add_widget(input_email)
         Settings_content.add_widget(user_content)
+        Settings_content.add_widget(self.btn_cam)
 
-        self.popup = Popup(title='', size_hint=(.520, .480), content=Settings_content, auto_dismiss=True)
+        self.popup = Popup(title='', size_hint=(.640, .480), content=Settings_content, auto_dismiss=True)
         self.popup.open()
     
     def show_popup_sim(self,*args):
         
-        btn_sim_start = IconButton(source='./icons/play.png', text='', size_hint_x=None, width=50)
-        btn_sim_start.bind(on_release=self.start)
+        self.btn_sim_start = IconButton(source='./icons/play.png', text='', size_hint_y = None, height = '48dp')
+        self.btn_sim_start.bind(on_release=self.sim_start)
         
-        btn_sim_save = IconButton(source='./icons/save.png', text='', size_hint_x=None, width=50)
+        btn_sim_save = IconButton(source='./icons/save.png', text='', size_hint_y = None, height = '48dp')
         btn_sim_save.bind(on_release=self.save_movie)
-
-        Settings_content = GridLayout(cols=1, row_force_default=True, row_default_height=40)
-        Settings_content.add_widget(btn_sim_start)
-        Settings_content.add_widget(btn_sim_save)
+        
+        image_dm = Image(source='./icons/dm.gif')
+        image_de = Image(source='./icons/de.gif')
+        
+        slider_dm = Slider(min= 0.0, max= 1.0, value = 0.25, step = 0.25, orientation='vertical', value_track=True, value_track_color=[1, 0, 0, 1], size_hint_y = None, height = '160dp')
+        slider_de = Slider(min= 0.0, max= 1.0, value = 0.75, step = 0.25, orientation='vertical', value_track=True, value_track_color=[1, 0, 0, 1], size_hint_y = None, height = '160dp')
+        
+        label_dm = Label(text = 'Dark Matter')
+        label_de = Label(text = 'Dark Energy')
+        label_png = Label(text = 'Early Universe')
+        label_gvr = Label(text = 'Gravity Type')
+        
+        self.spinner_dm = Spinner(text='Select', values=('Cold', 'Hot'))
+        self.spinner_de = Spinner(text='Select', values=('Constant', 'Quintessence', 'Phantom'))
+        self.spinner_png = Spinner(text='Select', values=('Gaussian', 'Negative Non-Gaussain', 'Positive Non-Gaussian'))
+        self.spinner_gvr = Spinner(text='Select', values=('Enistein', 'Modified Gravity'))
+        
+        Settings_content = BoxLayout(orientation='horizontal')
+        
+        slider_dm_content = GridLayout(cols=1)
+        slider_dm_content.add_widget(image_dm)
+        slider_dm_content.add_widget(slider_dm)
+        slider_dm_content.add_widget(self.btn_sim_start)
+        
+        slider_de_content = GridLayout(cols=1)
+        slider_de_content.add_widget(image_de)
+        slider_de_content.add_widget(slider_de)
+        slider_de_content.add_widget(btn_sim_save)
+        
+        subSettings_content = GridLayout(cols=2, size_hint_x = None, width=350)
+        subSettings_content.add_widget(label_dm)
+        subSettings_content.add_widget(self.spinner_dm)
+        subSettings_content.add_widget(label_de)
+        subSettings_content.add_widget(self.spinner_de)
+        subSettings_content.add_widget(label_png)
+        subSettings_content.add_widget(self.spinner_png)
+        subSettings_content.add_widget(label_gvr)
+        subSettings_content.add_widget(self.spinner_gvr)
+        
+        Settings_content.add_widget(subSettings_content)
+        Settings_content.add_widget(slider_dm_content)
+        Settings_content.add_widget(slider_de_content)
+        
     
-        self.popup_sim = Popup(title='', size_hint=(.450, .250), content=Settings_content, auto_dismiss=True)
+        self.popup_sim = Popup(title='', size_hint=(.680, .460), content=Settings_content, auto_dismiss=True)
         self.popup_sim.open()
     
     def show_popup_cam(self, *args):
@@ -159,7 +207,7 @@ class MyApp(App):
         self.popup.open()
     
     def clean(self, *args):
-        self.ax.clear(); self.ax.axis('off'); self.ax.imshow(self.img); self.canvas.draw()
+        self.ax.clear(); self.ax.axis('off'); self.ax.imshow(self.img); self.canvas.draw(); self.Settings_Page.remove_widget(self.btn_pause)
     
     def camera(self, *args):
         self.cam.play = not self.cam.play
@@ -167,7 +215,7 @@ class MyApp(App):
         self.cam.export_to_png("./tmp/image.png")
         self.btn_cam.source = "./tmp/image.png"
     
-    def start(self, *args):
+    def sim_start(self, *args):
         #self.progress_var.set(0); self.frames = 0; self.maxframes = 0
         
         Simu_Dir = "./../Dens-Maps/"
@@ -180,27 +228,26 @@ class MyApp(App):
         def animate(filename):
             image = np.load(filename); indx = filenames.index(filename)#; image=ndimage.gaussian_filter(image, sigma= sigmaval, truncate=truncateval, mode='wrap')
             im.set_data(image + 1)#; im.set_clim(image.min()+1.,image.max()+1.)
-            #self.time.set_text('%s %s' %(round(lktime[indx], 4), text_dict['t49']))
-            self.time.set_text('%s %s' %(round(lktime[indx], 4), 'time: '))
+            self.time.set_text('%s %s' %(round(lktime[indx], 4), text_dict['t49']))
             return im
         
         dens_map = np.load(filenames[0])#;  dens_map=ndimage.gaussian_filter(dens_map, sigma= sigmaval, truncate=truncateval, mode='wrap') #; dens_map0 = load(filenames[-1]); #print dens_map0.min()+1, dens_map0.max()+1.
         im = self.ax.imshow(dens_map + 1, cmap=cm.magma, norm=colors.LogNorm(vmin=1., vmax=1800., clip = True), interpolation="bicubic")#, clim = (1, 1800.+1.))
         
-        self.ax.annotate('This Universe is made by ' + self.input_name.text, xy=(0.25, 0.45), fontsize='12', fontstyle = 'oblique', color='white', xycoords='data', xytext=(10., 40.), textcoords='data')
-        self.time = self.ax.text(0.1, 0.05 , 'time' + ' %s Gyr' %round(z[0], 4), horizontalalignment='left', verticalalignment='top',color='white', transform = self.ax.transAxes, fontsize=10)
+
+        self.time = self.ax.text(0.1, 0.05 , text_dict['t43'] + ' %s Gyr' %round(lktime[0], 4), horizontalalignment='left', verticalalignment='top',color='white', transform = self.ax.transAxes, fontsize=10)
 
 
         arr_hand = mpimg.imread(CWD + "/tmp/" + "image.png")
-        imagebox = OffsetImage(arr_hand, zoom=.08); xy = [0.30, 0.45] # coordinates to position this image
+        imagebox = OffsetImage(arr_hand, zoom=.1); xy = (0.1, 0.15) # coordinates to position this image
 
-        ab = AnnotationBbox(imagebox, xy, xybox=(50., -70.), xycoords='data', boxcoords="offset points", pad=0.1)
+        ab = AnnotationBbox(imagebox, xy, xybox=(0.1, 0.15), xycoords='axes fraction', boxcoords='axes fraction', pad=0.1)
         self.ax.add_artist(ab)
 
         
         arr_hand1 = mpimg.imread("./icons/simcode.png")
-        imagebox1 = OffsetImage(arr_hand1, zoom=.1); xy = [950.0, 85.0] # coordinates to position this image
-        ab1 = AnnotationBbox(imagebox1, xy, xybox=(0., 0.), xycoords='data', boxcoords="offset points", pad=0.1)
+        imagebox1 = OffsetImage(arr_hand1, zoom=.1); xy = (0.9, 0.9) # coordinates to position this image
+        ab1 = AnnotationBbox(imagebox1, xy, xybox=(0.9, 0.9), xycoords='axes fraction', boxcoords='axes fraction', pad=0.1)
         self.ax.add_artist(ab1)
 
         #iMpc = lambda x: x*1024/125  #x in Mpc, return in Pixel *3.085e19
@@ -208,15 +255,28 @@ class MyApp(App):
         self.ax.add_artist(ob)
 
 #        sim_details_text = '%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s' %(text_dict['t42'], self.Name_Var.get(), text_dict['t53'], self.SC_Type, text_dict['t20'], self.DE_type, text_dict['t24'], self.DM_type, text_dict['t27'],  self.EU_Type, text_dict['t31'] , self.MG_Type)
-#        print sim_details_text
-#        self.ax.text(0.18, 0.85, sim_details_text, color='white', bbox=dict(facecolor='none', edgecolor='white', boxstyle='round,pad=1', alpha=0.5), transform = self.ax.transAxes, alpha = 0.5)
+
+        sim_details_text = '%s: %s\n%s: %s\n%s: %s\n%s: %s\n%s: %s' %(text_dict['t42'], self.input_name.text, text_dict['t53'], self.spinner_de.text, text_dict['t24'], self.spinner_dm.text, text_dict['t27'],  self.spinner_png.text, text_dict['t31'] , self.spinner_gvr.text) #, self.SC_Type, text_dict['t20']
+        print sim_details_text
+        self.ax.text(0.1, 0.83, sim_details_text, color='white', bbox=dict(facecolor='none', edgecolor='white', boxstyle='round,pad=1', alpha=0.5), transform = self.ax.transAxes, alpha = 0.5)
 
         self.ani = animation.FuncAnimation(self.fig, animate, filenames, repeat=False, interval=25, blit=False)
         self.ax.axis('off'); self.ax.get_xaxis().set_visible(False); self.ax.get_yaxis().set_visible(False); self.canvas.draw()
         
 #        self.progress["value"] = 0; self.maxframes = 300; self.progress["maximum"] = 300
 #        self.read_frames()
+        self.Settings_Page.add_widget(self.btn_pause)
         self.popup_sim.dismiss()
+        
+    def pause(self, *args):
+        if self.anim_running:
+            self.btn_pause.source = './icons/play.png'
+            self.ani.event_source.stop()
+            self.anim_running = False
+        else:
+            self.btn_pause.source = './icons/pause.png'
+            self.ani.event_source.start()
+            self.anim_running = True
 
     def save_movie(self, *args):
         writer = animation.writers['ffmpeg'](fps=15)#, bitrate=16000, codec='libx264')
